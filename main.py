bf9d9dace25c6fa43b21a1754315f1156efed098
Brayan <161508666+BrayanCharlesEtec@users.noreply.github.com>
2026-08-05 17:52:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
-#======================================================================================================================================================================================================
-#======================================================================================================================================================================================================
-# app=FastAPI(title=" primeira API evex.")
-# personalizar o nome.
-#======================================================================================================================================================================================================
-#======================================================================================================================================================================================================
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import Base, engine, get_db
+from models import ProdutoDB
+from schemas import ProdutoCreate, ProdutoResponse
+
+Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
 app = FastAPI()
 
-@app.get('/')
-def principal():
-    return {'mensagem': 'minha primeira API em fastAPI!'} 
+@app.get('/produtos', response_model=list[ProdutoResponse])
+def listar_produtos(db: Session = Depends(get_db)):
+    return db.query(ProdutoDB).all()
 
-@app.get('/sobre')
-def sobre():
-    return {'mensagem': 'pagina sobre!'} 
+@app.post('/produtos', response_model=ProdutoResponse, status_code=201)
+def criar_produto(produto: ProdutoCreate, db: Session = Depends(get_db)):
+    novo_produto = ProdutoDB(**produto.dict())
+    db.add(novo_produto)
+    db.commit()
+    db.refresh(novo_produto)
+    return novo_produto
