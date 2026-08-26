8eb3f5826820ae0818bfe180b1f72dc8e49b4a7a
Brayan <161508666+BrayanCharlesEtec@users.noreply.github.com>
2026-08-26 16:18:21 -0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -14,3 +14,18 @@ class ProdutoUpdate(ProdutoBase):
 class ProdutoResponse(ProdutoBase):
     id: int
     model_config = ConfigDict(from_attributes=True)
+    
+class SaopauloBase(BaseModel):
+    nome: str = Field(min_length=1, max_length=100)
+    titulos: int
+    cores: str
+    idade: int
+
+
+class SaopauloCreate(SaopauloBase):
+    pass
+
+
+class SaopauloResponse(SaopauloBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
