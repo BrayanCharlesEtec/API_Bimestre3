8ef0ce55e907dd59310b65b55e600f5b66152fc1
Brayan <161508666+BrayanCharlesEtec@users.noreply.github.com>
2026-08-26 16:18:01 -0300

    Update models.py

--- models.py
+++ models.py
@@ -6,3 +6,11 @@ class ProdutoDB(Base):
     nome = Column(String(100), nullable=False)
     preco = Column(Float, nullable=False)
     quantidade = Column(Integer, nullable=False)
+class SaopauloDB(Base):
+    __tablename__ = 'saopaulo'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(100), nullable=False)
+    titulos = Column(Integer, nullable=False)
+    cores = Column(String(100), nullable=False)
+    idade = Column(Integer, nullable=False)
